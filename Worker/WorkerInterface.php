d3df430cf8afc76acddf124b5bbe20ff921833b4
Alexander Keil <alexander.keil80@gmail.com>
2019-02-14 10:13:15 +0100

    Update WorkerInterface.php

--- Worker/WorkerInterface.php
+++ Worker/WorkerInterface.php
@@ -2,6 +2,9 @@
 
 namespace WorkerBundle\Worker;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 /**
  * @author Alexander Keil (alexanderkeil@leik-software.com)
  */
@@ -12,4 +15,8 @@ interface WorkerInterface
     public function getMessages(): array;
 
     public function canRun(array $options): bool;
+
+    public function setInput(InputInterface $input): void;
+
+    public function setOutput(OutputInterface $output): void;
 }
