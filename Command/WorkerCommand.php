6bd7209bdfcd8ae927e84abe18092fcdac2dd231
Alexander Keil <alexander.keil80@gmail.com>
2019-09-18 07:42:40 +0200

    Implemented lock feature in command

--- Command/WorkerCommand.php
+++ Command/WorkerCommand.php
@@ -3,6 +3,7 @@
 namespace WorkerBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use WorkerBundle\Worker\WorkerInterface;
@@ -13,6 +14,8 @@ use Symfony\Component\Console\Input\InputOption;
  */
 class WorkerCommand extends Command
 {
+    use LockableTrait;
+
     protected $workers = [];
 
     public function addWorker(WorkerInterface $worker): void
@@ -42,6 +45,11 @@ class WorkerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
+        if(!$this->lock()){
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
         /** @var WorkerInterface $worker */
         foreach ($this->workers as $worker) {
             $worker->setInput($input);
@@ -61,6 +69,8 @@ class WorkerCommand extends Command
                 $output->writeln(
                     sprintf('Exception in Worker "%s": "%s" in File "%s" on %d', \get_class($worker), $exception->getMessage(), $exception->getFile(), $exception->getLine())
                 );
+            }finally{
+                $this->release();
             }
         }
     }
